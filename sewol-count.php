25b57a1cca7b33df20759752aa654bc4c3b1ea35
pangol <pangol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 02:42:08 +0000

    add tags and brances
    git-svn-id: https://plugins.svn.wordpress.org/sewol-count/tags/1.0@1167485 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sewol-count.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: Sewol Count
+Plugin URI: http://parkyong.com
+Description: count day after Sewol Ferry Disaster
+Version: 1.0
+Author: Park Yong
+Author URI: http://parkyong.com
+License: GPLv2 or later
+Text Domain: sewol-count
+Domain Path: /languages
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+add_action( 'widgets_init', 'sewol_register_widgets' );
+
+function sewol_register_widgets() {
+	register_widget( 'sewol_widget' );
+}
+
+class sewol_widget extends WP_Widget {
+
+	function sewol_widget () {
+		$widget_ops = array( 'classname' => 'sewol_widget',
+			'description' => __( 'Count day after Sewol Ferry Disaster', 'sewol-plugin' ));
+		$this->WP_Widget( 'sewol_widget', __('Sewol Count Widget', 'sewol-plugin'), $widget_ops );
+	}
+
+	function form ( $instance ) {
+		$defaults = array( 'title' => __('Sewol', 'sewol-plugin'));
+
+		$instance = wp_parse_args( (array)$instance, $defaults );
+		$title = strip_tags( $instance['title'] );
+		?>
+		<p>
+			<?php _e('Title', 'pp-plugin' ) ?>:
+			<input class="widefat"name="<?php echo $this->get_field_name('title'); ?>"
+			type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+	<?php
+	}
+
+	function update ( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags(esc_attr($new_instance['title']));
+
+		return $instance;
+	}
+
+	function widget ( $args, $instance ) {
+
+		echo $before_widget;
+		echo '<aside id="sewol" class="widget">';
+
+		$title = apply_filters('widget_title', $instance['title']);
+		if ( !empty( $title ) ) { 
+			echo '<h1 class="widget-title" >' . $before_title . $title . $after_title . '</h1>';
+		}
+
+		$now = time(); 
+		$dday = mktime(0,0,0,4,16,2014); 
+		$xday = ceil(($now-$dday)/(60*60*24)); 
+		echo '<p id="sewol" style="color:yellow">' . $xday . '</p>';
+
+		echo '</aside>';
+		echo $after_widget;
+
+	}
+}?>
